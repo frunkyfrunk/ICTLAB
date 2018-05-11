5a9ab415e65953794985a1cc21804f205f109079
frunkyfrunk <verhagen.frank@gmail.com>
2018-05-11 17:46:28 +0200

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <div v-for="story in stories" class="story">
+    <div class="col-md-12">
+            <h2 class="text-center">Import user stories</h2>
+            <hr class="hidden-xs">
+        </div>
+    <div v-for="story in stories" class="story row">
+<div class="col-md-9">
       <div class="form-group">
       <div class="input-group">
     <div class="input-group-prepend">
         <span class="input-group-text">As a </span>
     </div>
-     <input class="form-control col-md-3" v-model="story.role" placeholder="Role"/>
+     <input class="form-control col-md-6" v-model="story.role" placeholder="Role"/>
     </div></div><div class="form-group">
     <div class="input-group"><div class="input-group-prepend">
         <span class="input-group-text">I </span>
@@ -15,7 +20,7 @@
      <select class="form-control col-md-2"><option>Want to</option>
      <option>Would like to</option>
      <option>Need to</option></select>
-     <input class="form-control col-md-7" v-model="story.goal" placeholder="Goal"/></div></div>
+     <input class="form-control col-md-9" v-model="story.goal" placeholder="Goal"/></div></div>
      <div class="form-group">
      <div class="input-group">
     <div class="input-group-prepend">
@@ -24,6 +29,11 @@
     </div>
     <input v-model="story.reason" class="form-control col-md-8" placeholder="Reason"/></div></div>
 </div>
+<div class="col-md-3">
+  <button type="button" class="btn btn-primary btn-lg">Save story</button>
+</div>
+    </div>
+    
     </div>
     
 </template>
