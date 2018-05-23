eccc0ef604c98b1d71361985d6d4782dc96370d6
Kevin Kroos <kevinkroos@gmail.com>
2018-05-23 13:26:08 +0200

    Added average score

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -75,7 +75,74 @@
                 </div>
                 <div v-if="step == 4" class="tab-pane active" role="tabpanel">
                   <h3>Results</h3>
-                  <div class="row">
+                  <div class="row">aaaaaaaaaaaaaaaaa
+
+
+    <div class="result container">
+                        <div class="row">
+           <!--  <div class="col-md-6">
+               <div class="panel-heading board">
+                  <textarea readonly type="text" class="cardtext form-control" placeholder="Userstory" aria-describedby="basic-addon1"> Average userstories score</textarea>
+                </div>
+            </div> -->
+            <div class="col-md-6"> 
+               <div id="specificChart" class="donut-size">
+                  <div class="pie-wrapper">
+                    <span class="label">
+                      <span class="num">{{averageUserstoriesScore}}</span><span class="smaller">%</span>
+                    </span>
+                    <div v-if="averageUserstoriesScore < 50" class="pie" style="clip: rect(auto, auto, auto, auto);">
+                      <div :style="{ transform: 'rotate('+(360 * (averageUserstoriesScore / 100)) +'deg)'}" class="left-side half-circle" style="border-width: 0.1em;transform: rotate(180deg);"></div>
+                      <div :style="{ transform: 'rotate('+ 180 +'deg)'}" class="right-side half-circle" style="border-width: 0.1em;"></div>
+                    </div>
+                    <div v-else class="pie" style="clip: rect(0, 1em, 1em, 0.5em);">
+                      <div :style="{ transform: 'rotate('+(360 * (averageUserstoriesScore/ 100)) +'deg)'}" class="left-side half-circle" style="border-width: 0.1em;transform: rotate(180deg);"></div>
+                      <div :style="{ transform: 'rotate('+ 0 +'deg)'}" class="right-side half-circle" style="border-width: 0.1em;"></div>
+                    </div>
+        
+                    <div class="shadow" style="border-width: 0.1em;"></div>
+                    </div>                    
+                </div>
+                <center><span>Users Average</span></center>
+              </div>
+
+
+      <div class="col-md-6"> 
+               <div id="specificChart" class="donut-size">
+                  <div class="pie-wrapper">
+                    <span class="label">
+                      <span class="num">{{averageUserstoriesScore}}</span><span class="smaller">%</span>
+                    </span>
+                    <div v-if="averageUserstoriesScore < 50" class="pie" style="clip: rect(auto, auto, auto, auto);">
+                      <div :style="{ transform: 'rotate('+(360 * (averageUserstoriesScore / 100)) +'deg)'}" class="left-side half-circle" style="border-width: 0.1em;transform: rotate(180deg);"></div>
+                      <div :style="{ transform: 'rotate('+ 180 +'deg)'}" class="right-side half-circle" style="border-width: 0.1em;"></div>
+                    </div>
+                    <div v-else class="pie" style="clip: rect(0, 1em, 1em, 0.5em);">
+                      <div :style="{ transform: 'rotate('+(360 * (averageUserstoriesScore/ 100)) +'deg)'}" class="left-side half-circle" style="border-width: 0.1em;transform: rotate(180deg);"></div>
+                      <div :style="{ transform: 'rotate('+ 0 +'deg)'}" class="right-side half-circle" style="border-width: 0.1em;"></div>
+                    </div>
+        
+                    <div class="shadow" style="border-width: 0.1em;"></div>
+                    </div>                    
+                </div>
+                <center><span>Your Average</span></center>
+              </div>
+
+<!--     <div class="col-md-12">
+      <h4>Tags</h4>
+      <span class="badge badge-info" v-for="tag in card.tags.data" :key="tag.id">{{tag.replace('"}','').replace('.','')}}</span>
+    </div> -->
+<!--     <div class="col-md-12">
+      <h4>Penalties</h4>
+      <div v-for="suggestion in card.suggestions.data.suggestions" :key="suggestion.id" class="alert alert-danger" role="alert">
+        <b>- {{suggestion.penaltypoints}}</b> {{suggestion.message}}
+      </div>
+  </div> -->
+                      </div>
+        </div>
+
+
+
                   <div class="col-md-12" v-for="card in lambdaCards" 
                       :key="card.id" 
                     >
@@ -130,6 +197,10 @@ export default {
   name: "Home",
   data() {
     return {
+      averageUserstoriesScore:0,
+      totalBoardScore:0,
+      totalBoardCards:0,
+
       selected: "",
       newCardName: "",
       selectedList: "",
@@ -280,10 +351,12 @@ export default {
         this.refreshData();
       }
       if (this.step == 4) {
+        this.getAverageScoreLambda();
         this.lambdaCards = [];
         this.cards.forEach(element => {
+          // console.log(element.name);
           this.getLambdas(element.name, element.id);
-        });
+        }); 
       }
     },
     previousstep() {
@@ -293,9 +366,27 @@ export default {
       this.getAllCardsOfSingleBoard();
       this.getListsFromBoards();
     },
+    async getAverageScoreLambda(){
+      
+      var averageUserstoriesScore;
+
+      await axios
+        .get(
+          "https://cd5zq44552.execute-api.eu-central-1.amazonaws.com/dev/myTrelloService/getAllcardsAverage"
+        )
+        .then(function(response) {
+
+          averageUserstoriesScore = response.data.data;
+        })
+        .catch(error => error);
+
+          this.averageUserstoriesScore = averageUserstoriesScore;
+    },
     async getLambdas(story, id) {
       var tags;
       var suggestions;
+      var test = 0;
+      var totalBoardScore = 0;
       await axios
         .post(
           "https://cd5zq44552.execute-api.eu-central-1.amazonaws.com/dev/myTrelloService/getStoryCategories",
@@ -316,15 +407,25 @@ export default {
           }
         )
         .then(function(response) {
+
+// test this
+// console.log(response.data.data.score);
+
+          // this.totalBoardScore += response.data.data.score);
+          
           suggestions = response.data;
         })
-        .catch(error => error);
+        .catch(error => error);        
+
       this.lambdaCards.push({
         id: id,
         name: story,
         suggestions: suggestions,
         tags: tags
       });
+    
+    // console.log(this.totalBoardScore);
+    
     }
   }
 };
--- src/components/partials/HeaderMenu.vue
+++ src/components/partials/HeaderMenu.vue
@@ -2,7 +2,7 @@
     <div class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
         
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Story Analysis</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -12,7 +12,7 @@
       <li class="nav-item active">
         <a class="nav-link" href="/">Home</a>
       </li>
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link" href="/Stories">Stories</a>
       </li>
       <li class="nav-item">
@@ -20,7 +20,7 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/Settings">Settings</a>
-      </li>
+      </li> -->
     </ul>
   </div></div></div></div>
 </template>
