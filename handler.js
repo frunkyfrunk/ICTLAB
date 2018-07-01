8a9121b211509e570ca34294bf030ea13c9324f9
frunkyfrunk <verhagen.frank@gmail.com>
2018-07-01 15:40:32 +0200

    initial

---
+++ handler.js
@@ -0,0 +1,173 @@
+'use strict';
+const request = require('request');
+const nlp = require('compromise')
+const StoryCalculatorService = require('./services/storyCalculateService.js');
+
+function saveStories(stories){
+
+  console.log('START LOGGIN DB CONNECTIONS!');
+  //  console.log(stories);
+
+var ObjStories = JSON.parse(stories);
+
+  for(var index in ObjStories) {
+    var data = [];
+    var storyName = ObjStories[index];
+    // var score = stories[index].score;
+    data.push(ObjStories);
+
+    console.log('DEBUG STORY NAME');
+    console.log(storyName);
+    data.push(100);
+    // var json_story = JSON.parse(data);
+    request.post({
+      url: 'https://unxe4qrdkk.execute-api.us-east-1.amazonaws.com/dev/stories',
+      body: storyName,
+      json: true
+    }, function(error, response, body){
+      console.log(body);
+    });
+  }
+}
+
+function calculateAverageScore(stories) {
+
+  var totalscore = 0;
+  var total = 0;
+
+  for (var i in stories) {
+
+    total++;
+
+    totalscore += stories[i].score;
+  }
+
+  return totalscore / total;
+}
+
+module.exports.getScore = (event, context, callback) => {
+  
+  // const data = calculateScore(event.body);
+  var dummyData  = '[{"story":"Ford"}, {"story":"BMW"}]';
+  var jsonData = JSON.parse(event.body);
+  var data = [];
+
+  // console.log('LOG STRING: ' + event.body);
+  // console.log('start loggin BODY propety');
+  // console.log(event.body);
+  
+
+  for (var i in jsonData) {
+    
+    // console.log('Start logging words to calc method');
+    // console.log(jsonData[i].story);
+    data.push(StoryCalculatorService.calculateScore(jsonData[i].story));
+    // console.log(data);
+  }
+
+  // saveStories(dummyData);
+
+  var averagescore = {
+    'averagescore': calculateAverageScore(data),
+  };
+
+  var aaaaa = JSON.stringify({'averagescore': calculateAverageScore(data)});
+
+  // data.push(averagescore);
+
+  const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({
+        data,
+        averagescore: calculateAverageScore(data),
+      }),        
+    };
+    callback(null,response);
+};
+
+module.exports.getStoryCategories = (event, context, callback) => {
+  
+  var dummyData  = '[{"story":"As user I want"}, {"story":"As Admininistrator i want dashboard"}]';
+  var jsonData = JSON.parse(event.body);
+  var data = [];
+
+    for (var i in jsonData) {
+    
+      data.push(StoryCalculatorService.storyCategories(jsonData[i].story));
+      console.log(jsonData[i].story);
+  }
+
+  // const data = storyCategories(event.body);
+
+  const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({
+       data
+      }),
+    };
+    callback(null,response);
+};
+
+
+module.exports.saveStories = (event, context, callback) => {
+  
+  var dummyData  = '[{"story":"As user I want"}, {"story":"As Admininistrator i want dashboard"}]';
+  var jsonData = JSON.parse(event.body);
+
+  console.log(jsonData)
+  
+  saveStories(event.body);
+
+  const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({
+       'status' : 'ok'
+      }),
+    };
+    callback(null,response);
+};
+
+module.exports.getAllcardsAverage = (event, context, callback) => {
+  
+  request.get('https://api.trello.com/1/members/me/cards?filter=all&key=32e0f2d3d2fc039ebace2c6d393b474d&token=0f4c965e894ab5c745329eb60fbe9bab9c132505efc64a7b51029cb483b75066', function (error, response, body) {
+     
+    var myData = JSON.parse(body);
+    var totalScore = 0;
+    var totalCards = 0;
+
+    for (var i in myData) {
+
+      totalCards++;
+      
+      var cardScore = StoryCalculatorService.calculateScore(myData[i].name).score;
+
+      totalScore += cardScore;
+    }
+
+    var data = Math.round(totalScore / totalCards);
+
+    response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({
+       data
+      }),
+    };
+    callback(null,response);
+  });    
+};
---
+++ services/storyCalculateService.js
@@ -0,0 +1,98 @@
+'use strict';
+const request = require('request');
+const nlp = require('compromise');
+
+class StoryCalculatorService {
+  //Get the analytics of the story
+  analyzeStory(story) {
+    let doc = nlp(story)
+    return {
+      wordcount: story.split(' ').length,
+      verbs: doc.verbs().length,
+      nouns: doc.nouns().length,
+      adjectives: doc.adjectives().length,
+      adverbs: doc.adverbs().length,
+      isquestion: (doc.questions().length > 0) ? true : false,
+      hasquotes: (doc.quotations().length > 0) ? true : false,
+    }
+  }
+
+  //Calculate the score of the story
+  calculateScore(analyzed, treshold) {
+    return {
+      lengthscore: analyzed.wordcount - 20 > 5 || analyzed.wordcount < 10 ? 10 - Math.abs(analyzed.wordcount - 20) : 10,
+      verbscore: analyzed.verbs > 5 ? 10 - analyzed.verbs : 10,
+      nounscore: analyzed.nouns > 3 ? 10 - analyzed.nouns : 10,
+      adjectivescore: analyzed.adjectives > 3 ? 10 - analyzed.adjectives : 10,
+      adverbscore: analyzed.adverbs > 2 ? 10 - analyzed.adverbs : 10,
+
+    }
+  }
+
+  //Get the suggestions for the story
+  getSuggestions(analyzed) {
+    let suggestions = [];
+    if (analyzed.isquestion) {
+      suggestions.push({
+        message: "Your user story shouldn't contain any questions"
+      })
+
+    }
+    if (analyzed.hasquotes) {
+      suggestions.push({
+        message: "Your user story shouldn't contain any quotes"
+      })
+    }
+
+    if (analyzed.wordcount - 20 > 5) {
+      suggestions.push({
+        message: "Your user story is too long. Please shorten it"
+      })
+    } else if (analyzed.wordcount < 10) {
+      suggestions.push({
+        message: "Your user story is too short. Please make it more descriptive"
+      })
+    }
+    if (analyzed.verbs > 5) {
+      suggestions.push({
+        message: "Don't use too much verbs in your user story. Try to shorten the story or split it up into multiple stories."
+      })
+    }
+    if (analyzed.adjectives > 3) {
+      suggestions.push({
+        message: "Don't use too much adjectives in your user story."
+      })
+    }
+    if (analyzed.nouns > 3) {
+      suggestions.push({
+        message: "We think that your user story contains too many different nouns. Try to make it a more singular story or split it into multiple stories."
+      })
+    }
+    if (analyzed.adverbs > 2) {
+      suggestions.push({
+        message: "Don't use too many adverbs. It's unnescessary and will make the user story harder to understand for the software engineers"
+      })
+    }
+    return suggestions
+  }
+
+  //Get the category tags for the story
+  getCategoryTags(story) {
+    let doc = nlp(story)
+    return doc.match("#Singular").match("!#Pronoun").out('array')
+  }
+
+  //Run this to get every result
+  main(story, treshold) {
+    var analyzed = this.analyzeStory(story)
+    return {
+      analyzed: analyzed,
+      score: this.calculateScore(analyzed, treshold),
+      suggestions: this.getSuggestions(analyzed),
+      categories: this.getCategoryTags(story)
+    }
+  }
+}
+
+module.exports = new StoryCalculatorService();
+console.log(module.exports.main("Don't use too many adverbs? It's unnescessary and will make the user story harder to understand for the software engineers"))
