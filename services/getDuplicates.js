a79eaf063422855dea6a4fcf5bf19b604b966b1c
frunkyfrunk <verhagen.frank@gmail.com>
2018-07-03 09:46:15 +0200

    test

--- services/getDuplicates.js
+++ services/getDuplicates.js
@@ -17,45 +17,6 @@ function parseStory(story) {
 }
 // console.log(parseStory("As a Manny’s food service customer, I need to save, copy, print, and email my list so that I can edit it again, check a received shipment against a printed list, and send the list to a restaurant."))
 
-<<<<<<< HEAD
-function checkAtomic(story){
-    var atomic = {
-        isAtomic: "",
-        atomicArray: []
-    }
-    var isAtomic;
-    var atomicArray = [];
-    const conjunctions = [' and ', '&', '\+', ' or ', '>', '<', '/', '\\']
-
-    var result = [];
-    for(var i = 0; i < conjunctions.length; i++){
-        if(nlp(story).match(conjunctions[i]).length != 0)
-            result.push(nlp(story).match(conjunctions[i]).out('offset'))
-    }
-    for(var i = 0; i < result.length; i++){
-        for(var x = 0; x < result[i].length; x++){
-            var resultObject = {
-            word:result[i][x].normal,
-            indexStart:result[i][x].wordStart,
-            indexEnd:result[i][x].wordEnd
-            }
-            atomic.atomicArray.push(resultObject);
-        }
-    }
-    if(atomic.atomicArray.length == 0){
-        atomic.isAtomic = true
-    }else {
-        atomic.isAtomic = false
-    }
-
-    return atomic;
-}
-console.log(checkAtomic("I am winning and I am faster, while being taller and stronger & live to the fullest + win"))
-console.log(checkAtomic("As a User, I am able to click a particular location from the map and thereby perform a search of landmarks associated with that latitude longitude combination"))
-console.log(checkAtomic("Hello world"))
-
-=======
->>>>>>> bf8a400fbd12bab6829a15cb7ec9bc57d4cf74af
 function calculateCosineDistance(story1, story2) {
   var sentence1 = story1.replace(/,/g, "").split(" ");
   var sentence2 = story2.replace(/,/g, "").split(" ");
