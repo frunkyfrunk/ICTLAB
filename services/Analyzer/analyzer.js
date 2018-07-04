f47c6a57d1c1e18de378b0e4760f8870b2b38866
frunkyfrunk <verhagen.frank@gmail.com>
2018-07-04 15:16:02 +0200

    added tags to nlpchecker

--- services/Analyzer/analyzer.js
+++ services/Analyzer/analyzer.js
@@ -1,21 +1,19 @@
 const nlp = require('compromise')
 const atomicChecker = require('./atomicChecker.js')
-const formChecker = require('./formChecker.js');
-const minimalChecker = require('./minimalChecker.js');
-//const spellChecker = require('./spellChecker.js');
-const nlpChecker = require('./nlpChecker.js');
+const formChecker = require('./formChecker.js')
+const minimalChecker = require('./minimalChecker.js')
+const nlpChecker = require('./nlpChecker.js')
 
 //insert user story and return analyzed object with all the details delivered
 function analyzeAll(story){
     result = {
         atomic: atomicChecker(story),
         form: formChecker(story),
-        //spellcheck:spellChecker(story),
         minimal:minimalChecker(story),
         nlp:nlpChecker(story)
     }
 
-    return result;
+    return result
 }
 module.exports = function (story) {
     return analyzeAll(story)
--- services/Analyzer/nlpChecker.js
+++ services/Analyzer/nlpChecker.js
@@ -1,14 +1,18 @@
 function analyzeWords(story) {
-    let doc = nlp(story)
-    return {
-        wordcount: story.split(' ').length,
-        verbs: doc.verbs().length,
-        nouns: doc.nouns().length,
-        adjectives: doc.adjectives().length,
-        adverbs: doc.adverbs().length,
-    }
+  let doc = nlp(story);
+  return {
+    wordcount: story.split(" ").length,
+    verbs: doc.verbs().length,
+    nouns: doc.nouns().length,
+    adjectives: doc.adjectives().length,
+    adverbs: doc.adverbs().length,
+    tags: doc
+      .match("#Singular")
+      .match("!#Pronoun")
+      .out("array")
+  };
 }
 
-module.exports = function (story) {
-    return analyzeWords(story)
+module.exports = function(story) {
+  return analyzeWords(story);
 };
